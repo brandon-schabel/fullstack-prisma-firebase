affcf4dca544a78e4681f6299c9e1926dbf5eef6
brandon-schabel <brandonschabel1995@gmail.com>
2019-05-19 18:15:54 -0700

    setup apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css"
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "react-apollo"
 
 import App from "./App"
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+})
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById("root")
 )
